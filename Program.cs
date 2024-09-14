a6ad0a9501189f3e6fe994253543e3885ac9a341
Miguel <dasilvamigueleduardo@gmail.com>
2024-09-14 17:07:23 -0300

    fix(models): correção de erro em models

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("3823242432", "Nokia X", "24234", 64);
+Iphone iphone = new Iphone("324344432", "Iphone 1334 geração", "2434", 1);
+
+nokia.Ligar();
+iphone.ReceberLigacao();
+
+nokia.InstalarAplicativo("Shadow Fight");
+iphone.InstalarAplicativo("Limpador de memoria");
